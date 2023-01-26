55fe99ca7180e5d84f8cf2d789bafd9c08213868
higorini <higormquinto@gmail.com>
2023-01-26 19:03:30 -0300

    Brand done

---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png';
+import atlassian from '../../assets/google.png';
+import dropbox from '../../assets/dropbox.png';
+import shopify from '../../assets/shopify.png';
+
+export {
+    google,
+    slack,
+    atlassian,
+    dropbox,
+    shopify,   
+}
