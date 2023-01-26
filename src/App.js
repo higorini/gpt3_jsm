dd238c2dbbedbae3b0ea4c4d91f5c9e300a92a4d
higorini <higormquinto@gmail.com>
2023-01-26 15:14:13 -0300

    Navbar finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+import './App.css'
 
 const App = () => {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { RiMenu3Line, RiCloseLin, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
+import logo from '../../assets/logo.svg';
+
+const Menu = () => (
+  <>
+    <p><a href='#home'>Home</a></p>
+    <p><a href='#wgtp3'>What is GPT3?</a></p>
+    <p><a href='#possibility'>Open AI</a></p>
+    <p><a href='#features'>Case Studies</a></p>
+    <p><a href='#blog'>Library</a></p>
+  </>
+)
 
 const Navbar = () => {
+  const [toogleMenu, setToogleMenu] = useState(false);
+
   return (
-    <div>
-      Navbar
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src={logo} alt='logo' />
+        </div>
+        <div className='gpt3__navbar-links_container'>
+          <Menu />
+        </div>
+      </div>
+      <div className='gpt3__navbar-sign'>
+        <p>Sign in</p>
+        <button type='button'>Sign up</button>
+      </div>
+      <div className='gpt3__navbar-menu'>
+        {toogleMenu
+          ? <RiCloseLine color='#ffffff' size={27} onClick={() => setToogleMenu(false)} />
+          : <RiMenu3Line color='#ffffff' size={27} onClick={() => setToogleMenu(true)} />
+        }
+        {toogleMenu && (
+          <div className='gpt3__navbar-menu_container scale-up-center'>
+            <div className='gpt3__navbar-menu_container-links'>
+              <Menu />
+              <div className='gpt3__navbar-menu_container-links-sign'>
+                <p>Sign in</p>
+                <button type='button'>Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
